14cf635ff75c094fa7dc3c429e746eb1ba3e4b94
venkata <prasanna.cvs@gmail.com>
2024-10-01 20:01:10 +0100

    Added Create SQL and Insert SQL for Employee table

---
+++ create_employees.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS MEMORY.DEFAULT.EMPLOYEE (
+    employee_id tinyint,
+    first_name varchar,
+    last_name varchar,
+    job_title varchar,
+    manager_id tinyint);
+
+
+ INSERT INTO MEMORY.DEFAULT.EMPLOYEE VALUES (1, 'Ian','James','CEO',4),
+                                            (2,'Umberto','Torrielli','CSO',1),
+                                            (3,'Alex','Jacobson','MD EMEA',2),
+                                            (4,'Darren','Poynton','CFO',2),
+                                            (5,'Tim','Beard','MD APAC',2),
+                                            (6,'Gemma','Dodd','COS',1),
+                                            (7,'Lisa','Platten','CHR',6),
+                                            (8,'Stefano','Camisaca','GM Activation',2),
+                                            (9,'Andrea','Ghibaudi','MD NAM',2);
+
+
